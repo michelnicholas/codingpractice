b9fc88ab7f80257c4d56cb13ba106230c7316d7d
Michel Nicholas <michelnicholas1993@gmail.com>
2021-04-18 13:05:35 -0400

    Rearranged problem question

--- src/test/java/stringsproblems/String2ZipZap.java
+++ src/test/java/stringsproblems/String2ZipZap.java
@@ -3,7 +3,10 @@ package stringsproblems;
 public class String2ZipZap {
 
     /*
-    Look for patterns like "zip" and "zap" in the string -- length-3, starting with 'z' and ending with 'p'. Return a string where for all such words, the middle letter is gone, so "zipXzap" yields "zpXzp".
+    Problem:
+
+    Look for patterns like "zip" and "zap" in the string -- length-3, starting with 'z' and ending with 'p'.
+    Return a string where for all such words, the middle letter is gone, so "zipXzap" yields "zpXzp".
 
     Examples:
 

e6b624c74d55e69b94b303d91102372f5000adb2
Michel Nicholas <michelnicholas1993@gmail.com>
2021-04-18 13:04:00 -0400

    Added examples to class

--- src/test/java/stringsproblems/String2ZipZap.java
+++ src/test/java/stringsproblems/String2ZipZap.java
@@ -2,6 +2,18 @@ package stringsproblems;
 
 public class String2ZipZap {
 
+    /*
+    Look for patterns like "zip" and "zap" in the string -- length-3, starting with 'z' and ending with 'p'. Return a string where for all such words, the middle letter is gone, so "zipXzap" yields "zpXzp".
+
+    Examples:
+
+    zipZap("zipXzap") → "zpXzp"
+
+    zipZap("zopzop") → "zpzp"
+
+    zipZap("zzzopzop") → "zzzpzp"
+     */
+
     public String zipZap(String str) {
         return null;
 

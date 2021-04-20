b8e117f148dffd4e0da55b3ee9c556b243719506
Michel Nicholas <michelnicholas1993@gmail.com>
2021-04-20 19:30:08 -0400

    created new class for String problem and solved

---
+++ src/test/java/stringsproblems/String2BobThere.java
@@ -0,0 +1,27 @@
+package stringsproblems;
+
+public class String2BobThere {
+    /*
+
+Return true if the given string contains a "bob" string, but where the middle 'o' char can be any char.
+
+
+bobThere("abcbob") → true
+bobThere("b9b") → true
+bobThere("bac") → false
+
+     */
+
+    public boolean bobThere(String str) {
+
+
+        for(int i = 0; i < str.length()-2;i++){
+            if(str.charAt(i) == 'b' && str.charAt(i+2) == 'b'){
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+}

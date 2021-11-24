1e002dd88dca87f019d640cdf2c8cdb8e300c68c
Michel Nicholas <michelnicholas1993@gmail.com>
2021-11-23 21:39:45 -0500

    added testng dependecy as well as solving the problem and adding an example method to showcase to terminal

--- src/test/java/stringsproblems/String1left2.java
+++ src/test/java/stringsproblems/String1left2.java
@@ -1,4 +1,35 @@
 package stringsproblems;
 
+import org.testng.annotations.Test;
+
 public class String1left2 {
+
+
+//    Given a string, return a "rotated left 2" version where the first 2 chars are moved to the end. The string length will be at least 2.
+//
+//
+//    left2("Hello") → "lloHe"
+//    left2("java") → "vaja"
+//    left2("Hi") → "Hi"
+
+    public String left2(String str){
+        if (str.length() < 2 ){
+            return str;
+        }
+        else{
+            String leftTwo = str.substring(0,2);
+            String rest = str.substring(2);
+            return rest + leftTwo;
+        }
+    }
+
+    @Test
+    public void example(){
+        System.out.println(left2("happy"));
+    }
+
+
+
+
+
 }

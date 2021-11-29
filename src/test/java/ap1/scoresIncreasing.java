7132cdcd3a369245233a080ebd35ead50d818219
Nicholas <zmichen@amazon.com>
2021-11-29 12:23:44 -0500

    completed first ap1 class and solved problem

---
+++ src/test/java/ap1/scoresIncreasing.java
@@ -0,0 +1,21 @@
+package ap1;
+
+public class scoresIncreasing {
+
+
+    public boolean scoresIncreasing(int[] scores){
+        // start with for loop to introduce
+        // scores.length-1 to make sure we close the paramaters of the array
+        for(int i = 0; i < scores.length-1;i++){
+
+            // create if statement filter to check to see if the number is greater than the next number
+            // return false
+            if(scores[i] > scores[i+1]){
+                return false;
+            }
+        }
+        // if all else fails we will return true
+        return true;
+    }
+
+}

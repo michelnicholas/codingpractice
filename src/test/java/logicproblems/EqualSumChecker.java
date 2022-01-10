239d068ff801a0ef71472127a2ad3911d45aeada
Michel Nicholas <michelnicholas1993@gmail.com>
2022-01-10 17:44:32 -0500

    finished equal sum checker problem and solved

---
+++ src/test/java/logicproblems/EqualSumChecker.java
@@ -0,0 +1,21 @@
+package logicproblems;
+
+public class EqualSumChecker {
+
+//    The method should return boolean and it needs to return true if
+//    the sum of the first
+//    and second parameter is equal to
+//    the third parameter.
+//    Otherwise, return false.
+
+
+    public  static boolean hasEqualSum(int num1 , int num2, int num3){
+        if (num1 + num2 == num3){
+            return true;
+        }
+        return false;
+    }
+
+
+
+}

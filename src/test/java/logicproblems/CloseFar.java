799c707394434f04fc4cd316e14a0b71bd14a777
Michel Nicholas <michelnicholas1993@gmail.com>
2022-01-31 19:07:45 -0500

    solved

---
+++ src/test/java/logicproblems/CloseFar.java
@@ -0,0 +1,26 @@
+package logicproblems;
+
+public class CloseFar {
+
+//    Given three ints, a b c, return true if one of b or c is "close" (differing from a by at most 1),
+//    while the other is "far", differing from both other values by 2 or more.
+//    Note: Math.abs(num) computes the absolute value of a number.
+//
+//    closeFar(1, 2, 10) → true
+//    closeFar(1, 2, 3) → false
+//    closeFar(4, 1, 3) → true
+//
+
+
+    public boolean closeFar(int a,int b, int c){
+// first we are going to want to see whether b or c is close
+        if (Math.abs(a-b) <=1 && Math.abs(a-c) >= 2 && Math.abs(b-c) >= 2){
+            return true;
+        }else if (Math.abs(a -c) <= 1 && Math.abs(a-b) >= 2 && Math.abs(b-c) >= 2){
+            return true;
+        }
+return false;
+
+
+    }
+}
--- src/test/java/logicproblems/closeFar.java
+++
@@ -1,4 +0,0 @@
-package logicproblems;
-
-public class closeFar {
-}
